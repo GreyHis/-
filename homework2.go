d48b5f1e4b9c9317f546f1f31a1bf3c88c5c8ff4
GreyHis <148050114+GreyHis@users.noreply.github.com>
2023-11-06 23:34:52 +0800

    Add files via upload

---
+++ homework2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sum(a int, b int) int {
+	return a + b
+}
+func sub(a int, b int) int {
+	return a - b
+}
+func multiply(a int, b int) int {
+	return a * b
+}
+func divide(a int, b int) int {
+	return a / b
+}
+func Calculator(a int, b int, CMD func(int, int) int) int {
+	return CMD(a, b)
+}
+func main() {
+	var c string
+	var a, b int
+	fmt.Println("请输入两个数和运算符号")
+	fmt.Scanf("%d %d %s", &a, &b, &c)
+	switch c {
+	case "sum":
+		fmt.Println(Calculator(a, b, sum))
+	case "sub":
+		fmt.Println(Calculator(a, b, sub))
+	case "multiply":
+		fmt.Println(Calculator(a, b, multiply))
+	case "divide":
+		fmt.Println(Calculator(a, b, divide))
+	}
+
+}
